98cfeb9ac6daf189c2ae6ee0242ba86fa76307e9
Bruno Lajoie <brunolajoie@gmail.com>
2017-05-18 13:56:58 +0200

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+
+  get 'cocktails/create'
+
+  get 'cocktails/new'
+
+  get 'cocktails/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  delete "doses/:id", to: "doses#destroy"
 end
+
+#            Prefix Verb   URI Pattern                                 Controller#Action
+#    cocktail_doses POST   /cocktails/:cocktail_id/doses(.:format)     doses#create
+# new_cocktail_dose GET    /cocktails/:cocktail_id/doses/new(.:format) doses#new
+#         cocktails GET    /cocktails(.:format)                        cocktails#index
+#                   POST   /cocktails(.:format)                        cocktails#create
+#      new_cocktail GET    /cocktails/new(.:format)                    cocktails#new
+#          cocktail GET    /cocktails/:id(.:format)                    cocktails#show
+#                   DELETE /doses/:id(.:format)                        doses#destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "TGV")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "tekila")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+mojito = Cocktail.where(name: "Mojito")
+tgv = Cocktail.where(name: "TGV")
+lemon = Ingredient.where(name: "lemon")
+ice = Ingredient.where(name: "ice")
+tekila = Ingredient.where(name: "tekila")
+gin = Ingredient.where(name: "gin")
+vodka = Ingredient.where(name: "vodka")
+ice = Ingredient.where(name: "ice")
+mint = Ingredient.where(name: "mint leaves")
+
+
+
+
